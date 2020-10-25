56b3613d832ef51cb135fd816ee147a9b952d372
Leonid <Leoon111@gmail.com>
2020-10-25 10:17:21 +0300

    Написал код парсера без присвоения полям, выводит все данные на консоль.

---
+++ Parser Console/Parser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using ExcelDataReader;
+
+namespace Parser_Console
+{
+    public class Parser
+    {
+        /// <summary>
+        /// Парсит файл .xls
+        /// </summary>
+        /// <param name="path">путь к файлу</param>
+        public void ExcelXLS(string pathFilePrice)
+        {
+            // todo добавить тест
+
+            // добавляем кодировку 1252 в Core
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            //var enc1252 = Encoding.GetEncoding(1252);
+
+            // тест контроль
+            Console.WriteLine(File.Exists(pathFilePrice) ? "File exists." : "File does not exist.");
+
+            if (File.Exists(pathFilePrice))
+            {
+                // Создаем поток для чтения.
+                var stream = File.Open(pathFilePrice, FileMode.Open, FileAccess.Read);
+                // В зависимости от расширения файла Excel, создаем тот или иной читатель.
+                // Читатель для файлов с расширением *.xlsx.
+                var excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                // Читатель для файлов с расширением *.xls.
+                // var excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+                // Читаем, получаем DataSet и работаем с ним как обычно.
+                var result = excelReader.AsDataSet();
+                // После завершения чтения освобождаем ресурсы.
+                excelReader.Close();
+
+                foreach (DataTable dt in result.Tables)
+                {
+                    Console.WriteLine(dt.TableName); // название таблицы
+                    // перебор всех столбцов
+                    foreach (DataColumn column in dt.Columns)
+                        Console.Write("\t{0}", column.ColumnName);
+                    Console.WriteLine();
+                    // перебор всех строк таблицы
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        // получаем все ячейки строки
+                        var cells = row.ItemArray;
+                        foreach (object cell in cells)
+                            Console.Write("\t{0}", cell);
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+        }
+    }
+}
--- Parser Console/Program.cs
+++ Parser Console/Program.cs
@@ -6,11 +6,16 @@ namespace Parser_Console
     {
         static void Main(string[] args)
         {
-            // открывает файл
+            // открывает файл // todo добавить автоматически выбор класса парсера по расширению файла
+            Parser parser = new Parser();
+            parser.ExcelXLS(@"E:\OneDrive\РАБОТА\Риэлтор\Застройщики\ССК Прайс 2020.10.23.xls");
+            //parser.ExcelXLS(@"E:\OneDrive\РАБОТА\Риэлтор\Застройщики\Железно, квартиры 2020.10.23.xlsx");
 
             // распарсивает по заданным алгоритмам
 
             // возвращает данные
+
+            Console.ReadLine();
         }
     }
 }
